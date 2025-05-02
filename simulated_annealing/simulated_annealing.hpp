715e28395c51a61f72f3725948aa59751dbd3b00
ei1333 <ei1333hobby@gmail.com>
2025-05-03 00:38:58 +0900

    焼きなまし (#3)
    * add sa
    
    * やきわすれ
    
    * add document

---
+++ simulated_annealing/simulated_annealing.hpp
@@ -0,0 +1,25 @@
+namespace SimulatedAnnealing {
+template<typename State>
+concept SAState = requires(State s, double delta, double progress)
+{
+  { s.update(delta, progress) } -> std::same_as<void>;
+};
+template<SAState State>
+void simulated_annealing(State &state, const double start_temp, const double end_temp, const int end_milliseconds) {
+  const Timer timer;
+  XorShift rng;
+  const auto start_time = timer.get_milliseconds();
+  const auto end_time = start_time + end_milliseconds;
+  while (true) {
+    const auto now = timer.get_milliseconds();
+    if (now >= end_time) {
+      break;
+    }
+    double progress = static_cast<double>(now - start_time) / end_milliseconds;
+    const double temp = start_temp + (end_temp - start_temp) * progress;
+    for (int i = 0; i < 256; i++) {
+      state.update(temp * log(rng.probability()), progress);
+    }
+  }
+}
+}
