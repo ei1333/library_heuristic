99517ae57c4ed784d27f2c4d6e47ff96be54fcd1
ei1333 <ei1333hobby@gmail.com>
2025-05-05 01:44:34 +0900

    add minimax (#10)
    * add minimax
    
    * fix

---
+++ minimax/minimax.hpp
@@ -0,0 +1,48 @@
+namespace MiniMax {
+template<class State>
+concept MiniMaxState =
+    requires(State &s,
+             const State &cs,
+             typename State::Action &a,
+             function<void(const typename State::Action &)> push)
+    {
+      typename State::Action;
+      typename State::Cost;
+
+      { cs.expand(push) } -> same_as<void>;
+      { cs.is_finished() } -> same_as<bool>;
+      { cs.evaluate() } -> same_as<typename State::Cost>;
+      { s.apply(a) } -> same_as<void>;
+      { s.rollback(a) } -> same_as<void>;
+    } &&
+    totally_ordered<typename State::Cost>;
+
+template<MiniMaxState State>
+pair<typename State::Action, typename State::Cost> minimax(State &state, const size_t depth) {
+  using Action = typename State::Action;
+  using Evaluator = typename State::Evaluator;
+  using Cost = typename Evaluator::Cost;
+  
+  assert(not state.is_finished());
+
+  vector<Action> candidates;
+  state.expand([&](const Action &a) { candidates.emplace_back(a); });
+  Cost best_score = numeric_limits<Cost>::min();
+  Action best_action;
+  for (const auto &action : candidates) {
+    state.apply(action);
+    Cost score;
+    if (depth <= 1 or state.is_finished()) {
+      score = -state.evaluate();
+    } else {
+      score = -minimax(state, depth - 1).second;
+    }
+    if (score > best_score) {
+      best_score = score;
+      best_action = action;
+    }
+    state.rollback(action);
+  }
+  return {best_action, best_score};
+}
+}
